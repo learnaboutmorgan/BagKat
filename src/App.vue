b02c2a0aaeee54969e10d50b0cbfc19e01000be1
learnaboutmorgan <morgan.oh@vanderbilt.edu>
2017-12-06 22:57:31 -0600

    made it nicer

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,81 @@
 <template>
   <div id="app">
-        <p>{{message}}</p>
-        <textarea v-model="message"></textarea>
-        <button @click="addItem()">CREATE YO SELF</button>
-        <ol>
+    <div> 
+        <div>
+          <button class="button" v-if="!creating" @click="poemify()">+POEMIFY</button>
+          <template v-else>
+            <textarea class="texty" v-model="message"></textarea>
+            <div>
+          <button class="button" @click="addItem()">CREATE YO SELF</button>
+          </div>
+          </template>
+       
+        </div>
+        <div>
           <todo-item v-for="(todo, index) in todos" :key="index" :todo="todo" @save="saveItems()" @delete="deleteItem(index)"/>
-        </ol>
+        </div>
     </div>
+  </div>
 </template>
+<style>
 
+
+body {
+  padding: 2px 2px;
+  margin: 2px 2px;
+  background: #dedede;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+}
+
+.texty {
+  width: 45%;
+  height: 200px;
+  margin: 7.5px;
+  padding: 12px 20px;
+  box-sizing: border-box;
+  border: 2px solid #ccc;
+  border-radius: 4px;
+  background-color: #f8f8f8;
+  font-size: 16px;
+  resize: none;
+}
+
+.editify {
+  width: 320px;
+  height: 200px;
+  box-sizing: border-box;
+
+  background: #fcfcfc;
+  border: none;
+
+  padding: 5px;
+  margin: 0px;
+  resize: none;
+  font-size: 16px;
+  background: #fcfcfc;
+  display: inline-block;
+  word-wrap: break-word;
+  white-space: pre-wrap;
+  overflow: scroll;
+}
+.button-lines {
+  margin-left: 49%;
+  display: inline-block;
+}
+.button {
+  background-color: #4caf50; /* Green */
+  border: none;
+  color: white;
+  padding: 2px 2px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 12px;
+  margin: 2px 2px;
+  cursor: pointer;
+}
+</style>
 <script>
 import TodoItem from "./components/TodoItem.vue";
 
@@ -25,23 +92,28 @@ export default {
   },
   data() {
     return {
+      creating: false,
       message: "",
       status: true,
       todos: initialTodos
     };
   },
   methods: {
+    poemify() {
+      this.creating = true;
+    },
     addItem() {
       if (this.message.length > 0) {
-        this.todos.push({ text: this.message });
+        this.todos.unshift({ text: this.message });
         this.message = "";
         this.saveItems();
       }
+      this.creating = false;
     },
     deleteItem(index) {
-       console.log("gterfd")
-        this.todos.splice(index, 1);
-        this.saveItems();
+      console.log("gterfd");
+      this.todos.splice(index, 1);
+      this.saveItems();
     },
     saveItems() {
       localStorage.setItem("todos", JSON.stringify(this.todos));
@@ -49,7 +121,3 @@ export default {
   }
 };
 </script>
-
-<style>
-
-</style>
--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,16 +1,59 @@
 <template>
-    <li>
-        {{ todo.text }}
-        <button @click="$emit('delete')">X</button>
-        <button @click="reverse()">REVERSE</button>
-        <button v-if="!editing" @click="edit()">EDIT</button>
-        <template v-else>
-            <button @click="save()">SAVE</button>
-            <textarea v-model="draft" />
-        </template>
-    </li>
-</template>
 
+    <div class="outter-todo-card">
+        <div class="todo-card">
+            <template v-if="!editing">
+                 {{ todo.text }}
+            </template>
+            <template v-else>
+             <textarea class="editify" v-model="draft"/>
+            </template>
+    
+        </div>
+        <div class="button-lines">
+        <button class="button" @click="$emit('delete')">DELETE</button>
+        <button class="button" @click="reverse()">REVERSE</button>
+        <button  class="button" v-if="!editing" @click="edit()">EDIT</button>
+        <template v-else>
+
+            <button  class="button" @click="save()">SAVE</button>
+        </template>
+        </div>
+        </div>
+    
+  
+</template>
+<style>
+
+
+.outter-todo-card {
+  width: 350px;
+  height: 250px;
+  box-sizing: border-box;
+
+  background: #fcfcfc;
+  box-shadow: 0px 7.5px 15px rgba(0, 0, 0, 0.2);
+  border-radius: 4px;
+  padding: 10.5px;
+  display: inline-block;
+  margin: 7.5px 15px;
+}
+.todo-card {
+  width: 320px;
+  height: 200px;
+  box-sizing: border-box;
+
+  background: #fcfcfc;
+
+  border-radius: 4px;
+  padding: 5px;
+  margin: 0px;
+  display: inline-block;
+  word-wrap: break-word;
+  white-space: pre-wrap;
+  overflow: scroll;
+}
+</style>
 <script>
 export default {
   props: {
