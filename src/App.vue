c0a13588c295c6e8a0eb322382af938a005ff39b
learnaboutmorgan <morgan.oh@vanderbilt.edu>
2017-12-10 23:34:53 -0600

    more poems added

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
           <button class="button" v-if="!creating" @click="poemify()">+POEMIFY</button>
           <template v-else>
            <textarea class="texty" v-model="message"></textarea>
-            <vue-markdown :source="message" ></vue-markdown>
+            <vue-markdown :source="message" />
           
             <div>
           <button class="button" @click="addItem()">CREATE YO SELF</button>
@@ -53,33 +53,33 @@ body {
 </style>
 <script>
 import TodoItem from "./components/TodoItem.vue";
-import VueEditor from 'vue-md-editor';
-import Vue from 'vue';
+import VueEditor from "vue-md-editor";
+import Vue from "vue";
 
 let initialTodos = [
   {
     text:
-      "My Ode to my country\n\nThis land, not mine\none I can’t claim but also can’t deny\n\ngluttony, processed food and hatred\nproducts of this country, to name a few\nmade in the U.S.A\n\nmy country tis of thee\nhowever not my sweet land of liberty\nnot my kind of empathy\nnot my dreams of being free\nand not where I can reap the opportunities\nthis country once promised me\n\nbut I have faith\nand can only wait \nand pray that one day we shall get over these\n\nWe still have more to go\nand until then I’ll stay by your side\nas we actually make America great again\nuntil we let go of our armors and our pride\n\nI won’t desert you just because I’m not on the winning side\n\nsomeone once told me,\n“if trump gets elected, I’m moving Canada”\n\nso easy for you to say\njust pack up all your stuff\nand move away \nmove to another country\npressing pause and play\nand bam, you’re in the best world again\n\nso easy for you to play this game\nbut when others come to this country,\nyou pull out your patriotism\n\nyou claim, \nBUILD THE WALL\nCRIMINALS\nRAPIST\nDRUGLORDS\n\nthe same old tricks\nyou used back in the old days\nplotting every way to make your gains\nthrough others’ pains\n\nyou leave this country \nbecause of the mess\nthis evil\nthis capitalism\nbut what you just ordered clothes from H&M and I don’t know what more capitalistic than online shopping\nthis hatred\nthis problematic state\nthis society that is all about the patriarchy\n\nand this hurts because you’re a male\nyou cry out yelling\n\nhow you feel discriminated\nhow you feel the pressure\nhow you feel judged \nfor what you wear\nand how you speak\n\nall while I try to tell you about women issues caused by THE SAME DAMN PATRIARCHY\n\nthere is no need to talk over me\nno need to make this a race\nthis is not a competition\n\nreally\nyou’re telling me what my priorities are supposed to be? For this society?\nseriously\n\ntoo easy to look at just yourself\nyour problems\nyour needs\n\nnow, if you’re a baby, I’d understand\nbut you\nyou’re a grown-ass man\n\nif you’re ‘man-enough’ to waggle your dick around\nlearn how to ‘man-enough’ your brain\nstop thinking with your dick and use that thing that’s in your head\n\nNo, I can’t leave\nI won’t leave \nI just go here\n\nThis is my home\nHome of the free\nI’ll fight for it\nI won’t back down\nthis is my country that I love\n\nleave and don’t come back\nfor if you do, I’ll be the one yelling: GO AWAY. THIS IS MY COUNTRY.\n"
+      "**The Alien**\nRocks\nCircle\nChildren\nShame\n\nWords instead of rocks\nSurrounding me are children \nFriends, I thought\nThey found a spot on my feather\nDifferent from them\nNo longer part of the flock \nThey’ve found a way to make me the Other\n\nSome say, “stick and stones may break my bones but names will never harm me”\n\nYou don’t know me\nNot when I’m carrying the card\nThe documentations\nThe legal papers\nMy right to live in this country called South Korea\n\nSymbol of the Foreigner\nBranded as the Other\nThe separation of u and s in us\nYou and the Stranger \nI’m the Stranger\n\nAll harmless until we start calling each other names\nUntil they start calling me names\n\nAlien.\nAlien.\nAlien.\n\nMy skin, face, and eyes say I am one of them\nmy tongue says otherwise\nThey hear the rhythm of English in my Korean\nThe sound of the short-tongued and accented\nNon-native\nLacking the crisp Korean intonation\nUnaware of the Korean wit\nCarrier of the Alien card\nThe visa\nProof that I am not from here\n\nAn immigrant minus the migration part\nBecause I’ve always lived here\nI was born here\nI don’t fit your box of norms\nor is it that I don’t fit your box of liminalities?\n\nThey say, go back to your star\nYou don’t belong here\nBut folks in the US won’t believe I’m from there either\nAnd so, let my stars stay high in the sky\nin its constellation \nLet them align with each other\nFill the gaps\npreviously occupied with what people thought I was supposed to be\nWith who I truly am\nBeyond a box \nBeyond a card\n\nI’m from here\nAnd I’m also from there\nthis is my home\nThis is my star\nDon’t tell me where I belong\n"
   },
   {
     text:
-      "Body of water\n\nI cannot live without my omma\nHow could she go about her life not seeing hers\nYears go by without seeing\nSeparated by water\nI don’t know\nThe truth is\nShe can’t\nIt’s eaten her up\n"
+      "### My Ode to my country\nThis land, not mine\none I can’t claim but also can’t deny\n\ngluttony, processed food and hatred\nproducts of this country, to name a few\nmade in the U.S.A\n\nmy country tis of thee\nhowever not my sweet land of liberty\nnot my kind of empathy\nnot my dreams of being free\nand not where I can reap the opportunities\nthis country once promised me\n\nbut I have faith\nand can only wait \nand pray that one day we shall get over these\n\nWe still have more to go\nand until then I’ll stay by your side\nas we actually make America great again\nuntil we let go of our armors and our pride\n\nI won’t desert you just because I’m not on the winning side\n\nsomeone once told me,\n“if trump gets elected, I’m moving Canada”\n\nso easy for you to say\njust pack up all your stuff\nand move away \nmove to another country\npressing pause and play\nand bam, you’re in the best world again\n\nso easy for you to play this game\nbut when others come to this country,\nyou pull out your patriotism\n\nyou claim, \nBUILD THE WALL\nCRIMINALS\nRAPIST\nDRUGLORDS\n\nthe same old tricks\nyou used back in the old days\nplotting every way to make your gains\nthrough others’ pains\n\nyou leave this country \nbecause of the mess\nthis evil\nthis capitalism\nbut what you just ordered clothes from H&M and I don’t know what more capitalistic than online shopping\nthis hatred\nthis problematic state\nthis society that is all about the patriarchy\n\nand this hurts because you’re a male\nyou cry out yelling\n\nhow you feel discriminated\nhow you feel the pressure\nhow you feel judged \nfor what you wear\nand how you speak\n\nall while I try to tell you about women issues caused by THE SAME DAMN PATRIARCHY\n\nthere is no need to talk over me\nno need to make this a race\nthis is not a competition\n\nreally\nyou’re telling me what my priorities are supposed to be? For this society?\nseriously\n\ntoo easy to look at just yourself\nyour problems\nyour needs\n\nnow, if you’re a baby, I’d understand\nbut you\nyou’re a grown-ass man\n\nif you’re ‘man-enough’ to waggle your dick around\nlearn how to ‘man-enough’ your brain\nstop thinking with your dick and use that thing that’s in your head\n\nNo, I can’t leave\nI won’t leave \nI just go here\n\nThis is my home\nHome of the free\nI’ll fight for it\nI won’t back down\nthis is my country that I love\n\nleave and don’t come back\nfor if you do, I’ll be the one yelling: GO AWAY. THIS IS MY COUNTRY.\n"
   },
   {
     text:
-      "Someone’s daughter\n\nI finally see my mom for more than my mom\nShe’s a daughter too \nAnd a sister\nI forgot she has an omma too\nImprinted in my mind, I auditioned and casted her for only one role in her film\nRole: mom\n\nLike I am her daughter, my mom is someone else’s\nShe’s part of a family\nBack in Korea\nWhere all her loved ones are, except for my sister and me\nAnd where she left them all, except for my sister and me\n\nShe laid down everything\nHer security\nHer education\nHer history\n\nFor she had a family\nA job to do\nA role to assume, for my sister and me\n\nThe more I think about it,\nThe more I feel guilty\nBurden\nI took her away\nSo there could be a chance for me to live\nTo be the star of her film, for me\n"
+      "Someone’s daughter\nI finally see my mom for more than my mom\nShe’s a daughter too \nAnd a sister\nI forgot she has an omma too\nImprinted in my mind, I auditioned and casted her for only one role in her film\nRole: mom\n\nLike I am her daughter, my mom is someone else’s\nShe’s part of a family\nBack in Korea\nWhere all her loved ones are, except for my sister and me\nAnd where she left them all, except for my sister and me\n\nShe laid down everything\nHer security\nHer education\nHer history\n\nFor she had a family\nA job to do\nA role to assume, for my sister and me\n\nThe more I think about it,\nThe more I feel guilty\nBurden\nI took her away\nSo there could be a chance for me to live\nTo be the star of her film, for me\n"
   },
   {
     text:
-      "My country tis of thee\n\nMy country tis of thee\nSweet land they promised me\nOh, I believed\nLand of insecurities\nLand of fallacies\nWrongly they portrayed of me\nLet me be free\n"
+      "My country tis of thee\nMy country tis of thee\nSweet land they promised me\nOh, I believed\nLand of insecurities\nLand of fallacies\nWrongly they portrayed of me\nLet me be free\n"
   },
   {
     text:
-      "A Benjamin Button\n\nI saw my mom become a child\nAs she went in to hug my grandma\nher omma, for the first time in years\n\nLast resort finally called us\nto come back to her motherland\nA painful one\nA dying one\n\nMy mom called for her omma \nlike how five-year-old me would\ncrying as I ran towards her with open arms\nreaching out as far as I can to grasp all of her\nScared\nAlone "
+      "A Benjamin Button\nI saw my mom become a child\nAs she went in to hug my grandma\nher omma, for the first time in years\n\nLast resort finally called us\nto come back to her motherland\nA painful one\nA dying one\n\nMy mom called for her omma \nlike how five-year-old me would\ncrying as I ran towards her with open arms\nreaching out as far as I can to grasp all of her\nScared\nAlone "
   },
   {
     text:
-      "Me against a cup of tea\n\nThey say I pick too many fights. \nBe smart and choose the right ones. \nSo I laugh it off. I play it cool. \nI do this for you.  \n\nGo along the jokes\nas I sip tea from a cute little china teacup\nwith my pinky up. \n\nMy tongue,\nscalding from the hot water. \n\nAt least this way, \nI’ll burn my words and hold my silence. \n\nI seal my lips so nothing can escape. \n\t\n\tThoughts. Emotions.\n\tThe Truth. \n\tFor you, I do this.\n\nBeat myself up. \nShut myself out.\n\nI was taught the right fight was to fight against myself.\nTrue me vs. \nthe me that they want to see.\n"
+      "Me against a cup of tea\nThey say I pick too many fights. \nBe smart and choose the right ones. \nSo I laugh it off. I play it cool. \nI do this for you.  \n\nGo along the jokes\nas I sip tea from a cute little china teacup\nwith my pinky up. \n\nMy tongue,\nscalding from the hot water. \n\nAt least this way, \nI’ll burn my words and hold my silence. \n\nI seal my lips so nothing can escape. \n\t\n>Thoughts. Emotions.\n>The Truth. \n>For you, I do this.\n\nBeat myself up. \nShut myself out.\n\nI was taught the right fight was to fight against myself.\nTrue me vs. \nthe me that they want to see.\n"
   }
 ];
 let storeTodos = localStorage.getItem("todos");
@@ -91,7 +91,7 @@ export default {
   name: "app",
   components: {
     TodoItem,
-     VueEditor
+    VueEditor
   },
   data() {
     return {
@@ -99,7 +99,7 @@ export default {
       message: "",
       status: true,
       todos: initialTodos,
-      source: new Date().toLocaleTimeString(),
+      source: new Date().toLocaleTimeString()
     };
   },
   methods: {
@@ -123,14 +123,10 @@ export default {
       localStorage.setItem("todos", JSON.stringify(this.todos));
     }
   },
-  mounted () {
+  mounted() {
     setInterval(() => {
       this.source = new Date().toLocaleTimeString();
     }, 1000);
   }
-
 };
-
-
-
 </script>
